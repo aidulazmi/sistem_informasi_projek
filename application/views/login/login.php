d1d94b1f08d4ce0eb285d143e5596a91bf85179a
Aidul Azmi. S <aidulazmi@gmail.com>
2021-09-23 15:36:08 +0700

    kl
    klk

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -14,7 +14,7 @@
 		<meta property="og:url" content="https://keenthemes.com/metronic" />
 		<meta property="og:site_name" content="Keenthemes | Metronic" />
 		<link rel="canonical" href="Https://preview.keenthemes.com/metronic8" />
-		<link rel="shortcut icon" href="<?=base_url();?>/asset/dist/assets/media/logos/favicon.ico" />
+		<link rel="shortcut icon" href="<?=base_url();?>/asset/dist/assets/media/logos/uin1.png" />
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 		<!--end::Fonts-->
@@ -33,8 +33,8 @@
 				<!--begin::Content-->
 				<div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
 					<!--begin::Logo-->
-					<a href="../../demo2/dist/index.html" class="mb-12">
-						<img alt="Logo" src="<?=base_url();?>/asset/dist/assets/media/logos/logo-1.svg" class="h-40px" />
+					<a href="#" class="mb-12">
+						<img alt="Logo" src="<?=base_url();?>/asset/dist/assets/media/logos/uin2.png" class="h-100px" />
 					</a>
 					<!--end::Logo-->
 					<!--begin::Wrapper-->
@@ -44,21 +44,21 @@
 							<!--begin::Heading-->
 							<div class="text-center mb-10">
 								<!--begin::Title-->
-								<h1 class="text-dark mb-3">Sign In to Metronic</h1>
+								<h1 class="text-dark mb-3">Sign In to SIPROJEK</h1>
 								<!--end::Title-->
 								<!--begin::Link-->
-								<div class="text-gray-400 fw-bold fs-4">New Here?
-								<a href="../../demo2/dist/authentication/flows/basic/sign-up.html" class="link-primary fw-bolder">Create an Account</a></div>
+								<div class="text-gray-400 fw-bold fs-4">Apakah Anda Karyawa
+								<a href="" class="link-primary fw-bolder">Login Terlebih Dahulu</a></div>
 								<!--end::Link-->
 							</div>
 							<!--begin::Heading-->
 							<!--begin::Input group-->
 							<div class="fv-row mb-10">
 								<!--begin::Label-->
-								<label class="form-label fs-6 fw-bolder text-dark">Email</label>
+								<label class="form-label fs-6 fw-bolder text-dark">Username</label>
 								<!--end::Label-->
 								<!--begin::Input-->
-								<input class="form-control form-control-lg form-control-solid" type="text" name="email" autocomplete="off" />
+								<input class="form-control form-control-lg form-control-solid" type="text" name="username" autocomplete="off" placeholder="Username" required="" />
 								<!--end::Input-->
 							</div>
 							<!--end::Input group-->
@@ -69,13 +69,10 @@
 									<!--begin::Label-->
 									<label class="form-label fw-bolder text-dark fs-6 mb-0">Password</label>
 									<!--end::Label-->
-									<!--begin::Link-->
-									<a href="../../demo2/dist/authentication/flows/basic/password-reset.html" class="link-primary fs-6 fw-bolder">Forgot Password ?</a>
-									<!--end::Link-->
 								</div>
 								<!--end::Wrapper-->
 								<!--begin::Input-->
-								<input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" />
+								<input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="off" placeholder="Password" required="" />
 								<!--end::Input-->
 							</div>
 							<!--end::Input group-->
@@ -88,40 +85,13 @@
 									<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
 								</button>
 								<!--end::Submit button-->
-								<!--begin::Separator-->
-								<div class="text-center text-muted text-uppercase fw-bolder mb-5">or</div>
-								<!--end::Separator-->
-								<!--begin::Google link-->
-								<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-								<img alt="Logo" src="<?=base_url();?>/asset/dist/assets/media/svg/brand-logos/google-icon.svg" class="h-20px me-3" />Continue with Google</a>
-								<!--end::Google link-->
-								<!--begin::Google link-->
-								<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100 mb-5">
-								<img alt="Logo" src="<?=base_url();?>/asset/dist/assets/media/svg/brand-logos/facebook-4.svg" class="h-20px me-3" />Continue with Facebook</a>
-								<!--end::Google link-->
-								<!--begin::Google link-->
-								<a href="#" class="btn btn-flex flex-center btn-light btn-lg w-100">
-								<img alt="Logo" src="<?=base_url();?>/asset/dist/assets/media/svg/brand-logos/apple-black.svg" class="h-20px me-3" />Continue with Apple</a>
-								<!--end::Google link-->
-							</div>
-							<!--end::Actions-->
 						</form>
 						<!--end::Form-->
 					</div>
 					<!--end::Wrapper-->
 				</div>
 				<!--end::Content-->
-				<!--begin::Footer-->
-				<div class="d-flex flex-center flex-column-auto p-10">
-					<!--begin::Links-->
-					<div class="d-flex align-items-center fw-bold fs-6">
-						<a href="https://keenthemes.com" class="text-muted text-hover-primary px-2">About</a>
-						<a href="mailto:support@keenthemes.com" class="text-muted text-hover-primary px-2">Contact</a>
-						<a href="https://1.envato.market/EA4JP" class="text-muted text-hover-primary px-2">Contact Us</a>
-					</div>
-					<!--end::Links-->
-				</div>
-				<!--end::Footer-->
+				
 			</div>
 			<!--end::Authentication - Sign-in-->
 		</div>
